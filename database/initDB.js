d66f4faa00361febe707e89b79c9cf48513f1fe7
Juanfrancisc <juanfrancisc@hotmail.com>
2022-09-12 17:37:17 +0200

    Añadido fichero Winframe

--- database/initDB.js
+++ database/initDB.js
@@ -24,9 +24,9 @@ async function main(){
         // Eliminar las tablas de la base de datos si existe
         console.log('Eliminando tablas si existen...');
         
-        await conexion.query(`DROP TABLE IF EXISTS user_view_exercises`);
+        await conexion.query(`DROP TABLE IF EXISTS user_like_exercises`);
         await conexion.query(`DROP TABLE IF EXISTS exercises`);
-        await conexion.query(`DROP TABLE IF EXISTS typology`);
+        //await conexion.query(`DROP TABLE IF EXISTS typology`);
         await conexion.query(`DROP TABLE IF EXISTS users`);
 
 
@@ -37,47 +37,56 @@ async function main(){
         console.log('Creando tabla de usuarios...');
         await conexion.query(`
             CREATE TABLE IF NOT EXISTS users (
-             id int unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
-             name varchar(15) NOT NULL,
-             email varchar(100) NOT NULL UNIQUE,
-             password varchar(15) NOT NULL,
-             type_user enum('normal','admin') NOT NULL)`
+                id int unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                name varchar(15) NOT NULL,
+                email varchar(100) NOT NULL UNIQUE,
+                password varchar(15) NOT NULL,
+                type_user enum('normal','admin') NOT NULL)`
         );
 
         console.log('Creando tabla de tipologias...');
-        await conexion.query(`
+        /*await conexion.query(`
             CREATE TABLE typology (
             id int unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
             title varchar(45) NOT NULL UNIQUE)`
-          );
+          );*/
 
         console.log('Creando tabla de ejercicios...');  
         await conexion.query(
             `CREATE TABLE exercises (
                 id int unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                idUser int unsigned NOT NULL
                 title varchar(45) NOT NULL UNIQUE,
                 description text NOT NULL,
                 id_typology int unsigned NOT NULL,
                 photo varchar(100) NOT NULL,
-                FOREIGN KEY (id_typology) REFERENCES typology (id)
-                ON DELETE CASCADE
+                typology enum('Natacion','Musculacion','Cardio','Relajacion') NOT NULL,
+                FOREIGN KEY (idUser) REFERENCES users(id)
               )`
         );
 
         console.log('Creando tabla de usuarios crea ejercicios...');
         await conexion.query(
-            `CREATE TABLE user_view_exercises (
+            `CREATE TABLE user_like_exercises (
                 id int unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
                 id_user int unsigned NOT NULL,
                 id_exercises int unsigned NOT NULL,
-                islike tinyint(1) NOT NULL DEFAULT '0',
-                favorites tinyint(1) NOT NULL DEFAULT '0',
+                FOREIGN KEY (id_exercises) REFERENCES exercises (id)
+                ON DELETE CASCADE
+              )`
+        );
+        /** Futura tabla de favoritos */
+        /*await conexion.query(
+            `CREATE TABLE user_favorites_exercises (
+                id int unsigned PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                id_user int unsigned NOT NULL,
+                id_exercises int unsigned NOT NULL,
                 FOREIGN KEY (id_user) REFERENCES users (id)
                 ON DELETE CASCADE,
                 FOREIGN KEY (id_exercises) REFERENCES exercises (id)
                 ON DELETE CASCADE
               )`
-        );
+        );*/
 
         console.log('Tablas creadas!');
 
@@ -99,24 +108,25 @@ async function main(){
                 ('user5', 'user5@user1.com', '12345');` 
         )
 
-        console.log('Insertando tipologias de ejemplo...');
+        /*console.log('Insertando tipologias de ejemplo...');
         await conexion.query(
             `INSERT INTO typology (title) VALUES ('Cardio'), ('Piscina'),('Relajación'),('Musculación');`
-        );    
+        );   */ 
 
         console.log('Insertando ejercicios de ejemplo...');
         await conexion.query(
-            `INSERT INTO exercises (title, description, id_typology, photo) VALUES ('Sentadillas', 'Realimos agachadillas hasta media altura y mantenemos unos segundos, nos ponemos ergidos y volvemos a comenzar. Unas 20 repeticiones con 10 segundos de descanso entre ellas', '1', 'foto.jpg')
-            ,('Abdominales', 'El ejercicio estándar de encogimiento abdominal se concentra en los músculos del estómago. Es un ejercicio eficaz y seguro que es ideal para principiantes, para ayudar a desarrollar músculos abdominales fuertes.', '1', 'foto.jpg')
-            ,('Flexiones', 'Las flexiones son un excelente ejercicio para trabajar los músculos del pecho y los brazos. Debido a la posición que usted tiene que mantener para hacer este ejercicio correctamente, también trabaja el núcleo, abdominales, piernas y espalda. Diferentes variaciones de las flexiones plantearán un nuevo desafío los mismos grupos musculares.', '4', 'foto.jpg')
-            ,('Nado a Crol', 'Este estilo es de forma alternada, mientras uno de los brazos del nadador se mueve en el aire con la palma hacia abajo dispuesta a ingresar al agua, y el codo relajado, el otro brazo avanza bajo el agua', '2', 'foto.jpg');`
+            `INSERT INTO exercises (title, description, id_typology, typology, photo) VALUES 
+            ('Sentadillas', 'Realimos agachadillas hasta media altura y mantenemos unos segundos, nos ponemos ergidos y volvemos a comenzar. Unas 20 repeticiones con 10 segundos de descanso entre ellas', '1', 'foto.jpg','Cardio')
+            ,('Abdominales', 'El ejercicio estándar de encogimiento abdominal se concentra en los músculos del estómago. Es un ejercicio eficaz y seguro que es ideal para principiantes, para ayudar a desarrollar músculos abdominales fuertes.', '1', 'foto.jpg','Musculacion')
+            ,('Flexiones', 'Las flexiones son un excelente ejercicio para trabajar los músculos del pecho y los brazos. Debido a la posición que usted tiene que mantener para hacer este ejercicio correctamente, también trabaja el núcleo, abdominales, piernas y espalda. Diferentes variaciones de las flexiones plantearán un nuevo desafío los mismos grupos musculares.', '4', 'foto.jpg','Cardio')
+            ,('Nado a Crol', 'Este estilo es de forma alternada, mientras uno de los brazos del nadador se mueve en el aire con la palma hacia abajo dispuesta a ingresar al agua, y el codo relajado, el otro brazo avanza bajo el agua', '2', 'foto.jpg','Natacion');`
         );
 
         console.log('Insertando datos de favorios de ejemplo....')
         await conexion.query(
-            `INSERT INTO user_view_exercises (id_user, id_exercises, islike, favorites) VALUES ('3', '2', '1','1'),
-            ('4', '1', '1','1'),
-            ('5', '4', '0','1');`
+            `INSERT INTO user_view_exercises (id_user, id_exercises) VALUES ('3', '2'),
+            ('4', '1'),
+            ('5', '4');`
         )
 
         console.log('Datos de ejemplo insertados!');
