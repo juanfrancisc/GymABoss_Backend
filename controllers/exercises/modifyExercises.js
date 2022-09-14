eeac18d4efd791d4b0b1fbe962d659d2c1751a69
davizdelarosa <108464629+davizdelarosa@users.noreply.github.com>
2022-09-14 16:32:35 +0200

    modify Exercises acabado

--- controllers/exercises/modifyExercises.js
+++ controllers/exercises/modifyExercises.js
@@ -14,34 +14,34 @@ const modifyExercises = async (req, res, next) => {
         const { title, description, typology, photo } = req.body;
 
         // Si no envia nada para editar, lanzaremos un error
-        if (!title || !description || !typology || !photo) {
+        if (!title && !description && !typology) {
             throw generateError('No se ha insertado ningún nuevo dato.', 400);
         }
 
         // Seleccionamos los datos antiguos del ejercicio
         const [exercise] = await connection.query(
-            `SELECT title, description, typology, photo FROM exercises WHERE id = ?`,
+            `SELECT title, description, typology FROM exercises WHERE id = ?`,
             [idExercise]
         );
 
         // Actualizamos la tabla exercises con los nuevos datos
         await connection.query(
             `UPDATE exercises 
-              SET title = ?,
-              description = ?
-              WHERE id = ?`,
+                SET title = ?,
+                description = ?,
+                typology = ?
+                WHERE id = ?`,
             [
                 title || exercise[0].title,
                 description || exercise[0].description,
                 typology || exercise[0].typology,
-                photo || exercise[0].photo,
                 idExercise,
             ]
         );
 
         res.send({
             status: 'Ok',
-            message: `El ejercicio con id ${idProduct} ha sido modificado con éxito!`,
+            message: `El ejercicio con id ${idExercise} ha sido modificado con éxito!`,
         });
     } catch (error) {
         next(error);
